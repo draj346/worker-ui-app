186812e2340884d1cbad18a082f8e24023e25af0
Dipesh Raj <draj346@gmail.com>
2025-07-03 22:49:51 +0530

    Updated bundle.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 const gulp = require('gulp');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
-const terser = require('gulp-terser');
 const rimraf = require('rimraf');
 const { watch, series } = require('gulp');
 
@@ -12,34 +10,28 @@ function clean(done) {
 }
 
 function build() {
-  return browserify('scripts/UI/index.js') // Entry point
-    .transform('babelify', {
-      extensions: ['.js'],
-      presets: ['@babel/preset-env'], // Inherits from .babelrc
-      plugins: ['transform-remove-console'] // Inherits from .babelrc
-    })
-    .bundle()
-    .on('error', function(err) {
-      console.error('Browserify error:', err.message);
-      this.emit('end');
-    })
-    .pipe(source('bundle.min.js'))
-    .pipe(buffer())
-    .pipe(terser({
-      compress: { ecma: 5 },
-      mangle: { toplevel: true },
-      format: { comments: false }
-    }))
-    .pipe(gulp.dest('dist'))
-    .on('end', () => {
-      console.log('Build completed at', new Date().toLocaleTimeString());
-    });
+  return browserify('scripts/UI/index.js', {
+    debug: true, // Generate source maps
+    plugin: [
+      // ES6 module support plugin
+      require('esmify') 
+    ]
+  })
+  .bundle()
+  .on('error', function(err) {
+    console.error('Bundling error:', err.message);
+    this.emit('end');
+  })
+  .pipe(source('bundle.js'))
+  .pipe(gulp.dest('dist'))
+  .on('end', () => {
+    console.log('ES6 bundle created at', new Date().toLocaleTimeString());
+  });
 }
-
 
 exports.clean = clean;
 exports.build = gulp.series(clean, build);
 exports.watch = function() {
-  console.log('Watching for changes...');
+  console.log('Watching for ES6 changes...');
   return watch('scripts/UI/**/*.js', { ignoreInitial: false }, series(clean, build));
 };
